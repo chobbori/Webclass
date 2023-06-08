6c9b2a6084a8a465b8800895b3dfbe40f356e076
Mona Lisa <in7144@naver.com>
2023-06-08 18:14:45 +0900

    web

---
+++ 38.제이쿼리/js/study4.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    let count = 0;
+    $(window).scroll(function() {
+        
+    })
+})
---
+++ 38.제이쿼리/js/study5.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    // $(".box").animate({width:400},1000).animate({height:400},1000)
+
+    // $(".box").stop().animate({width:400},1000).animate({height:400},1000
+
+
+    let b1top = $(".b1").offset().top
+    let b2top = $(".b2").offset().top
+    $("html,body").animate({top:b1top
+
+});
