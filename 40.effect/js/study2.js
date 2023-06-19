1cc3acbe1820b444616f9839fb275221536964be
Mona Lisa <in7144@naver.com>
2023-06-19 20:09:48 +0900

    web

---
+++ 40.effect/js/study2.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    let animationTrem = 0.1
+    let animationDuration = 5
+    //선택한 태그들은 배열형태ㅐ로 리턴된다 그래서 배열의 원소의 개수를 .length를 통해 구해볼 수 잇다
+
+    for(let i=0 ; i<14 ; i++) {
+        $(".floatingTxt h1 b:nth-of-type("+(i+1)+").css("Animation",""
+    }
+
+})
