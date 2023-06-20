39c793fc6620215bfcb4e1f38c62a4350966db5e
Mona Lisa <in7144@naver.com>
2023-06-20 17:13:24 +0900

    web

---
+++ 22.css-headerlayout/js/study3.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //gnd클래스에 마우스를 올리면 header태그에 on클래스가 추가되어서 커버가 깔리게 됨
+
+    //gnb클래스에서 마우스를 빼면 header태그에 on클래스가 삭제되어서 커버가 걷혀짐
+
+    $(".gnb").hover(function() {})
+})
--- 40.effect/js/study2.js
+++ 40.effect/js/study2.js
@@ -1,11 +1,27 @@
-$(document).ready(function() {
 
-    let animationTrem = 0.1
-    let animationDuration = 5
-    //선택한 태그들은 배열형태ㅐ로 리턴된다 그래서 배열의 원소의 개수를 .length를 통해 구해볼 수 잇다
+$(document).ready(function(){
 
-    for(let i=0 ; i<14 ; i++) {
-        $(".floatingTxt h1 b:nth-of-type("+(i+1)+").css("Animation",""
-    }
+    // let aniamtionTerm = 0.1
+    // let animationDuration = 5
+    // // console.log($(".floatingTxt h1 b").length) //선택한 태그들은 배열형태로 리턴된다 그래서 배열의 원소의 개수를 .length를 통해 구해볼 수 있다.
+
+    // for(let i=0 ; i<$(".floatingTxt h1 b").length ; i++){
+    //     $(".floatingTxt h1 b:nth-of-type("+(i+1)+")").css("animation","introAni "+animationDuration+"s ease "+(aniamtionTerm*i)+"s forwards")
+    // }
+
+
+    
+    textAni(".floatingTxt",{
+        aniDelay:0.2,
+        aniSpeed:0.7,
+        textSize:50
+    })
+
+    textAni(".txtbox",{
+        aniDelay:1,
+        aniSpeed:0.5,
+        textSize:90
+    })
+
 
 })
